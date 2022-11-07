31e12da909b83c863d13993941cafe48fc4f7143
sadmanador <gazitasnuva.nsu@gmail.com>
2022-11-07 20:59:22 +0600

    product-card needed to be adjusted

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './Product.css'
 
 const Product = ({ product }) => {
-
-    const { name, price, seller, rating, img } = product;
+console.log(product);
+    const { name, price, seller, ratings, img } = product;
     return (
         <div className='product-card'>
-            <img src={img} alt="" className='product-img'/>
+            <img src={img} alt={name} className='product-img' />
             <div className="product-info">
-                <h2>{name}</h2>
-                <h4>Price: ${price}</h4>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price: ${price}</p>
                 <p><small>Manufacturer: {seller}</small></p>
-                <p><small>Rating: {rating} stars</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
             </div>
+            <button className='add-to-cart'>
+                <p>Add to Cart</p>
+
+            </button>
         </div>
     );
 };
