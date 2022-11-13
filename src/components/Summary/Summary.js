0a4c04f930b1bf9087dac471bbe54e85d66d98c9
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-11-14 00:36:20 +0600

    summary section calculation begin

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -4,6 +4,10 @@ const Summary = () => {
     return (
         <div>
             <h1>From summary</h1>
+            <p>Total price: </p>
+            <p>Tax (10%): </p>
+            <p>Shipping Charges: </p>
+            <p>Grand total: </p>
         </div>
     );
 };
