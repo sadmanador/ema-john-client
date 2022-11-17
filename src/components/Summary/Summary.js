b7a32888f23a965e75cfaa724f0fa8eb2e7e3551
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-11-17 20:08:36 +0600

    cart calculation done!

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './Summary.css'
 
 const Summary = ({cart}) => {
+    let total =0;
+let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = total * 0.1;
+    const grandTotal = total + shipping + +tax
     return (
         <div className='summary-container'>
-            <h1>From summary: {cart.length}</h1>
-            <p>Total price: </p>
-            <p>Tax (10%): </p>
-            <p>Shipping Charges: </p>
-            <p>Grand total: </p>
+            <h3>From summary: {cart.length}</h3>
+            <p>Total price: ${total}</p>
+            <p>Tax (10%): ${tax.toFixed(2)}</p>
+            <p>Shipping Charges: ${shipping}</p>
+            <h5>Grand total: {grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
