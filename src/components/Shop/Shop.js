1934ddb97ab9ce8e6441e83a39f4a4d681b91dee
sadmanador <84454116+sadmanador@users.noreply.github.com>
2022-11-18 14:08:31 +0600

    module-49 done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -31,10 +31,21 @@ const Shop = () => {
         setCart(savedCard);
     }, [products]);
 
-    const handleAddToCart = product => {
-        const newCart = [...cart, product];
+    const handleAddToCart = selectedProduct => {
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if (!exists) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
+
         setCart(newCart);
-        addToLocalStorage(product.id);
+        addToLocalStorage(selectedProduct.id);
     }
 
 
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './Summary.css'
 
-const Summary = ({cart}) => {
-    let total =0;
-let shipping = 0;
-    for(const product of cart){
-        total = total + product.price;
+const Summary = ({ cart }) => {
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
 
     const tax = total * 0.1;
     const grandTotal = total + shipping + +tax
-    
+
     return (
         <div className='summary-container'>
-            <h3>From summary: {cart.length}</h3>
+            <h3>From summary: {quantity}</h3>
             <p>Total price: ${total}</p>
             <p>Tax (10%): ${tax.toFixed(2)}</p>
             <p>Shipping Charges: ${shipping}</p>
